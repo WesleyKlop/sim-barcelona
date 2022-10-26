cdf12d13d7faee235d57f8a9de256ebd5abc8580
Wesley Klop <wesley19097@gmail.com>
2022-10-26 13:50:45 +0200

    yolo

---
+++ public/assets/app.mjs
@@ -0,0 +1,11 @@
+document.write('Page loaded')
+
+const evtSource = new EventSource('/api/listen')
+evtSource.addEventListener('message', (evt) => {
+    alert('Got data!')
+    document.write(evt.data)
+})
+
+evtSource.addEventListener('error', (evt) => {
+    alert(`error: ${evt}`)
+})
--- server/blueprints/api.py
+++ server/blueprints/api.py
@@ -1,10 +1,23 @@
 from http import HTTPStatus
 
 from flask import Blueprint, request
+from server.logic.announcer import announcer
+from server.logic import format_sse
 
 api = Blueprint('api', __name__, url_prefix='/api')
 
 @api.route('/hello', methods=['GET'])
 def hello():
+    announcer.announce(format_sse('Hello, world!'))
     return 'Hello World!', HTTPStatus.OK
     
+@api.route('/listen', methods=['GET'])
+def listen():
+
+    def stream():
+        messages = announcer.listen()  # returns a queue.Queue
+        while True:
+            msg = messages.get()  # blocks until a new message arrives
+            yield msg
+
+    return flask.Response(stream(), mimetype='text/event-stream')
