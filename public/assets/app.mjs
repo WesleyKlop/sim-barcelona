f4cf81fd70a41eda29c70f066fb26e9c556d01ce
Wesley Klop <wesley19097@gmail.com>
2022-10-27 15:16:08 +0200

    Remove alert

--- public/assets/app.mjs
+++ public/assets/app.mjs
@@ -24,7 +24,6 @@ const setLoading = (isLoading) => {
 
 evtSource.addEventListener('phase', ({data}) => {
     console.log('New phase', data)
-    alert(data)
     phase = data
 
     switch(data) {
