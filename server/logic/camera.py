d257cd413832f2327570e7ef72c5b97521435a3a
Wesley Klop <wesley19097@gmail.com>
2022-10-27 13:45:26 +0200

    aaaaa

--- server/logic/camera.py
+++ server/logic/camera.py
@@ -2,19 +2,25 @@ from time import sleep
 
 from cv2 import VideoCapture, imwrite
 
+from .announcer import announcer
 from .fs import generate_random_filepath
 
+
 def take_picture(dest_dir: str = None):
+    announcer.log('Going to take a pic')
     cam = VideoCapture(0)
     # Take picture using usb webcam
     # Returns the path to the picture
     sleep(5)
+    announcer.log('Slept')
     s, img = cam.read()
+    announcer.log('got picture')
     if s:
         # Save the image
         path = generate_random_filepath(dest_dir)
         imwrite(path, img)
         print(path)
         return path
-    else:
-        print("Picture capture error")
+
+    print("Picture capture error")
+    return None
