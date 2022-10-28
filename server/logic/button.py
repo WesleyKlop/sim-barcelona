54586e2c4061dd9f842756915da4fd35f223561e
Wesley Klop <wesley19097@gmail.com>
2022-10-28 10:05:00 +0200

    Cleanup code a bit

--- server/logic/button.py
+++ server/logic/button.py
@@ -9,44 +9,42 @@ from server.logic.announcer import announcer
 from server.logic.camera import take_picture
 from server.logic.image import ImageGenerator
 
-BUTTON_PIN = 7
-
 
 class ButtonHandler(Thread):
-    def __init__(self, pin, func, edge='both', bouncetime=200):
+    def __init__(self, pin, func, edge='both', bounce_time=200):
         super().__init__(daemon=True)
 
         self.edge = edge
         self.func = func
         self.pin = pin
-        self.bouncetime = float(bouncetime) / 1000
+        self.bounce_time = float(bounce_time) / 1000
 
-        self.lastpinval = GPIO.input(self.pin)
+        self.last_pinval = GPIO.input(self.pin)
         self.lock = Lock()
 
     def __call__(self, *args):
         if not self.lock.acquire(blocking=False):
             return
 
-        t = Timer(self.bouncetime, self.read, args=args)
+        t = Timer(self.bounce_time, self.read, args=args)
         t.start()
 
     def read(self, *args):
         pinval = GPIO.input(self.pin)
 
         if (
-                ((pinval == 0 and self.lastpinval == 1) and
+                ((pinval == 0 and self.last_pinval == 1) and
                  (self.edge in ['falling', 'both'])) or
-                ((pinval == 1 and self.lastpinval == 0) and
+                ((pinval == 1 and self.last_pinval == 0) and
                  (self.edge in ['rising', 'both']))
         ):
             self.func(*args)
 
-        self.lastpinval = pinval
+        self.last_pinval = pinval
         self.lock.release()
 
 
-def do_the_thing(evt):
+def do_the_thing(_pin):
     log.info('Starting a run')
     announcer.sse('running', 'phase')
     generator = ImageGenerator()
@@ -56,6 +54,7 @@ def do_the_thing(evt):
     if img_path is None:
         announcer.log('Failed to take picture')
         return None
+
     announcer.sse('/results/' + basename(img_path), 'image')
     announcer.log('Got picture')
 
@@ -63,7 +62,9 @@ def do_the_thing(evt):
     announcer.log('Generated picture')
     announcer.sse('/results/' + result, 'image')
     announcer.sse('finished', 'phase')
-    last_timestamp = time()
+
+
+BUTTON_PIN = 7
 
 
 def setup():
@@ -71,6 +72,6 @@ def setup():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    cb = ButtonHandler(BUTTON_PIN, do_the_thing, edge='rising', bouncetime=100)
+    cb = ButtonHandler(BUTTON_PIN, do_the_thing, edge='rising', bounce_time=100)
     cb.start()
     GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING, callback=cb)
