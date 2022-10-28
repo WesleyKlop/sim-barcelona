6fe3a90f5c44776f6a8e650bac21f79bb135f885
Wesley Klop <wesley19097@gmail.com>
2022-10-28 10:47:29 +0200

    Show style image

--- public/assets/app.mjs
+++ public/assets/app.mjs
@@ -1,6 +1,7 @@
 const $log = document.querySelector('#log')
 const $result = document.querySelector('#result')
 const $loadingOverlay = document.querySelector('#loading-overlay')
+const $style = document.querySelector('#style')
 
 const evtSource = new EventSource('/api/listen')
 
@@ -42,6 +43,10 @@ evtSource.addEventListener('phase', ({data}) => {
 
 evtSource.addEventListener('image', ({data}) => {
     setImage(data)
+})
+
+evtSource.addEventListener('style', ({data}) => {
+    $style.src = data
 })
 
 evtSource.addEventListener('log', ({data}) => {
